e56e3c23cdfe97f6e67b13f01704fbef185f2fc6
Pablo Gaza <p.garza1983@gmail.com>
2024-02-23 22:35:12 +0100

    Update global CSS and header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -11,28 +11,24 @@ export default function Header() {
       <Container className="h-full flex items-center md:gap-x-5 justify-between md:justify-start">
         <Logo />
         {/* Search bar */}
-        <div className="group w-full hidden bg-primary md:flex items-center gap-x-1  rounded-full px-4 py-1.5 focus-within:border-lightText">
+        <div className="group w-full hidden bg-primary md:flex items-center gap-x-1  rounded-full px-4 py-1.5 focus-within:border-lightText focus-within:bg-lightText">
           <GoSearch className="text-lightText group-focus-within:text-darkText duration-200" />
           <input
             type="text"
             placeholder="Search for products"
-            className="text-lightText placeholder:text-sm placeholder:text-lightText flex-1 outline-none bg-primary focus:placeholder:text-darkText "
+            className="text-lightText placeholder:text-sm placeholder:text-lightText flex-1 outline-none bg-primary focus:placeholder:text-darkText focus:bg-lightText"
           />
         </div>
         {/* Login Button */}
-        <div className="bg-bgLight text-gray-500 flex items-center justify-center p-1.5 rounded-full hover:bg-white border-[1px] border-lightText hover:border-orange-500 duration-200">
+        <div className=" text-lightText  flex items-center justify-center p-1.5 rounded-full hover:text-lightText/30 duration-200">
           <RiUserShared2Line className="text-xl text-lightText" />
-          <p className="text-sm font-semibold ps-1">
-            Login/Register
-          </p>
+          <p className="text-sm ps-1">Login/Register</p>
         </div>
         {/* cart button */}
-        <div className="relative duration-200 border-[1px] hover:border-gray-300 bg-black hover:bg-bodyColor rounded-full text-slate-100 hover:text-slate-900 flex items-center justify-center gap-x-1 px-3 py-1.5">
+        <div className="relative bg-bodyColor rounded-full text-lightText flex items-center justify-center gap-x-1 px-3 py-1.5">
           <BsCart className="text-xl text-lightText" />
-          <p className="text-sm font-semibold ps-1">
-            $0.00
-          </p>
-          <span className="bg-white shadow-xl shadow-black text-orange-600 rounded-full text-sm font-semibold absolute -right-2 -top-1 w-5 h-5 flex items-center justify-center">
+          <p className="text-sm ps-1">$0.00</p>
+          <span className="bg-lightText shadow-xl shadow-black text-primary rounded-full text-sm font-semibold absolute -right-2 -top-1 w-5 h-5 flex items-center justify-center">
             0
           </span>
         </div>
