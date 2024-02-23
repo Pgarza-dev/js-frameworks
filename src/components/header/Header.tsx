657f7b90a63a5907033469ce78cff81892afd622
Pablo Gaza <p.garza1983@gmail.com>
2024-02-23 20:33:04 +0100

    Update layout and header styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,20 +7,20 @@ import { RiUserShared2Line } from "react-icons/ri";
 
 export default function Header() {
   return (
-    <header className="bg-bglight h-20 ">
+    <header className="bodyColor h-20 ">
       <Container className="h-full flex items-center md:gap-x-5 justify-between md:justify-start">
         <Logo />
         {/* Search bar */}
-        <div className="group w-full hidden bg-white md:flex items-center gap-x-1 border-[1px] border-lightText/50 rounded-full px-4 py-1.5 focus-within:border-orange-600">
-          <GoSearch className="text-gray-500 group-focus-within:text-darkText duration-200" />
+        <div className="group w-full hidden bg-primary md:flex items-center gap-x-1  rounded-full px-4 py-1.5 focus-within:border-lightText">
+          <GoSearch className="text-lightText group-focus-within:text-darkText duration-200" />
           <input
             type="text"
             placeholder="Search for products"
-            className="placeholder:text-sm flex-1 outline-none"
+            className="text-lightText placeholder:text-sm placeholder:text-lightText flex-1 outline-none bg-primary focus:placeholder:text-darkText "
           />
         </div>
         {/* Login Button */}
-        <div className="bg-bgLight text-gray-500 flex items-center justify-center p-1.5 rounded-full hover:bg-white border-[1px] border-gray-200 hover:border-orange-500 duration-200">
+        <div className="bg-bgLight text-gray-500 flex items-center justify-center p-1.5 rounded-full hover:bg-white border-[1px] border-lightText hover:border-orange-500 duration-200">
           <RiUserShared2Line className="text-xl text-lightText" />
           <p className="text-sm font-semibold ps-1">
             Login/Register
--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Logo() {
   return (
     <Link href={"/"}>
-      <h3 className=" text-3xl font-semibold hover:text-orange-500 cursor-pointer duration-200">
+      <h3 className=" text-3xl font-semibold text-lightText hover:text-lightText/30 cursor-pointer duration-200">
         RainyDays
       </h3>
     </Link>
