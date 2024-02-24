d929644498f1f4eaf5def839ebaec9298f7a9c62
Pablo Gaza <p.garza1983@gmail.com>
2024-02-24 14:18:23 +0100

    Update banner and bannerText components

--- src/components/bannerText/bannerText.tsx
+++ src/components/bannerText/bannerText.tsx
@@ -10,11 +10,35 @@ export default function BannerText({
   title,
 }: BannerTextProps) {
   return (
-    <div className="hidden lg:inline-block absolute top-0 left-0 w-full h-full">
-      <Container className="flex flex-col gap-y-6 justify-center">
-        <h2 className="text-7xl font-bold text-red">
+    <div className="hidden sm:inline-block md:inline-block lg:inline-block absolute top-0 left-0 w-full h-full">
+      <Container className="flex flex-col gap-y-6 justify-center h-full">
+        <motion.h2
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          className="text-7xl font-bold text-white">
           {title}
-        </h2>
+        </motion.h2>
+        <motion.p
+          initial={{ y: 40, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          className="text-lg text-lightText">
+          Urban Edge: Authentic street fashion. <br />
+          Shop the latest trends.
+        </motion.p>
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.7 }}
+          className="flex gap-x-4 mt-2 z-50">
+          <button className="relative py-3  px-6 rounded-full cursor-pointer duration-200 text-white border-[1px] border-lightModePrimary hover:bg-lightModePrimary">
+            Find out more
+          </button>
+          <button className="py-3 px-6 rounded-full bg-lightModeBody cursor-pointer text-lightModeText hover:bg-lightModePrimary hover:text-white duration-200">
+            Shop now
+          </button>
+        </motion.div>
       </Container>
     </div>
   );
