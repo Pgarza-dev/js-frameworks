f9aa078ca9791bb75163590fd42349c8aa71ae09
Pablo Gaza <p.garza1983@gmail.com>
2024-02-24 09:24:56 +0100

    Update header component styling

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -11,24 +11,24 @@ export default function Header() {
       <Container className="h-full flex items-center md:gap-x-5 justify-between md:justify-start">
         <Logo />
         {/* Search bar */}
-        <div className="group w-full hidden bg-lightModePrimary md:flex items-center gap-x-1  rounded-full px-4 py-1.5 focus-within:border-lightModeText focus-within:bg-lightModeText">
-          <GoSearch className="text-lightModeText group-focus-within:text-darkText duration-200" />
+        <div className="group w-full hidden border-2 md:flex items-center gap-x-1 bg-white rounded-full px-4 py-1.5 focus-within:border-lightModePrimary focus-within:bg-white">
+          <GoSearch className="text-lightModeText group-focus-within:text-darkText duration-200 " />
           <input
             type="text"
             placeholder="Search for products"
-            className="text-lightModeText placeholder:text-sm placeholder:text-lightModeText flex-1 outline-none bg-lightModePrimary focus:placeholder:text-darkText focus:bg-lightModeText"
+            className="text-lightModeText placeholder:text-sm placeholder:text-lightModeText flex-1 outline-none bg-white focus:placeholder:text-darkText focus:bg-white"
           />
         </div>
         {/* Login Button */}
-        <div className=" text-lightModeText  flex items-center justify-center p-1.5 rounded-full hover:text-lightModeText/30 duration-200">
+        <div className=" text-lightModeText border-2 bg-white flex items-center justify-center p-1.5 rounded-full hover:border-2 hover:border-lightModePrimary cursor-pointer hover:text-lightModeText/30 duration-200">
           <RiUserShared2Line className="text-xl text-lightModeText" />
           <p className="text-sm ps-1">Login/Register</p>
         </div>
         {/* cart button */}
-        <div className="relative bg-lightModeBody rounded-full text-lightModeText flex items-center justify-center gap-x-1 px-3 py-1.5">
-          <BsCart className="text-xl text-lightModeText" />
+        <div className="relative bg-lightModeText rounded-full text-lightText flex items-center justify-center gap-x-1 px-3 py-1.5">
+          <BsCart className="text-xl text-lightText" />
           <p className="text-sm ps-1">$0.00</p>
-          <span className="bg-lightModeText shadow-xl shadow-black text-lightModePrimary rounded-full text-sm font-semibold absolute -right-2 -top-1 w-5 h-5 flex items-center justify-center">
+          <span className="bg-lightModeBody shadow-xl shadow-black text-orange-500 rounded-full text-sm font-semibold absolute -right-2 -top-1 w-5 h-5 flex items-center justify-center">
             0
           </span>
         </div>
