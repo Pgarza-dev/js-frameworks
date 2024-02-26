da8fee4e77bcdc93f3ba2dcbbd6f9bb27edbab09
Pablo Gaza <p.garza1983@gmail.com>
2024-02-26 09:24:34 +0100

    Update banner component styling

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -15,8 +15,8 @@ export default function Banner() {
     return (
       <div
         onClick={onClick}
-        className="p-2 bg-white  hover:text-lightModePrimary hover:font-bold cursor-pointer  rounded-full text-2xl items-center justify-center z-20 absolute left-1/3  bottom-10">
-        <PiCaretLeftLight />
+        className="p-2 bg-white hover:font-bold hover:text-lightModePrimary cursor-pointer duration-200 rounded-full text-2xl items-center justify-center z-20 absolute right-1/3 bottom-10">
+        <PiCaretRightLight />
       </div>
     );
   };
@@ -25,8 +25,8 @@ export default function Banner() {
     return (
       <div
         onClick={onClick}
-        className="p-2 bg-white hover:font-bold hover:text-lightModePrimary cursor-pointer duration-200 rounded-full text-2xl items-center justify-center z-20 absolute right-1/3 bottom-10">
-        <PiCaretRightLight />
+        className="p-2 bg-white  hover:text-lightModePrimary hover:font-bold cursor-pointer  rounded-full text-2xl items-center justify-center z-20 absolute left-1/3  bottom-10">
+        <PiCaretLeftLight />
       </div>
     );
   };
