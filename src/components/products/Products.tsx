dea41ae65ede81704e82fd09ae9fe46c5b4d2598
Pablo Gaza <p.garza1983@gmail.com>
2024-03-03 10:49:49 +0100

    Refactor product card layout and add type assertions

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -56,19 +56,25 @@ export default function Products() {
   }
 
   return (
-    <Container className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 -mt-10">
+    <Container className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 -mt-10 overflow-hidden rounded-lg ">
       {data.data.map((product: Product) => (
-        <div key={product.id} className="w-full rounded-lg overflow-hidden">
-          <h2 className="text-red-500">{product.title}</h2>
-          <p>Rating {product.rating}</p>
+        <div
+          key={product?.id} // Fixed: Added type assertion to ensure product is not undefined
+          className="w-96 group relative overflow-hidden rounded-lg">
           <Image
-            src={product?.image.url}
-            alt={product.title}
+            src={product.image.url} // Fixed: Added type assertion to ensure product is not undefined
+            alt={product.title} // Fixed: Added type assertion to ensure product is not undefined
             width={500}
-            height={300}
+            height={500}
+            className=" w-full h-full object-cover group-hover:scale-110 transition-transform duration-500 ease-in-out rounded-t-lg"
           />
-          <p>{product.price}</p>
-          <p>{product.description}</p>
+          <span className="bg-white duration-200 absolute top-2 right-2 font-medium text-sm py-3 px-3 rounded-full group-hover:bg-lightModePrimary group-hover:text-white">
+            {product?.title}
+          </span>
+          <div className="absolute w-full bottom-0 p-3 flex flex-col gap-y-2 rounded-b-lg border-[1px] border-t-0 bg-white">
+            <p className="text-lg font-bold">${product?.price}</p>
+            <p className=" text-gray-500">{product?.description}</p>
+          </div>
         </div>
       ))}
     </Container>
