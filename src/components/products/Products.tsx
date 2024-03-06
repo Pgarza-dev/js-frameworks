9df438a66823860f628687dce26a72f9169d51ce
Pablo Gaza <p.garza1983@gmail.com>
2024-03-06 10:53:18 +0100

    Commented out the "Add to cart" button in Products component

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -117,9 +117,9 @@ export default function Products() {
                 <p>Reviews ( {product?.reviews.length} )</p>
               </div>
 
-              <button className="border-[1px] px-2 py-3 border-bg-lightModePrimary text-sm hover:border-white hover:text-lightModePrimary hover:shadow-none shadow-md tracking-wide">
+              {/* <button className="border-[1px] px-2 py-3 border-bg-lightModePrimary text-sm hover:border-white hover:text-lightModePrimary hover:shadow-none shadow-md tracking-wide">
                 Add to cart
-              </button>
+              </button> */}
             </div>
             {/* <div>
               <p className=" text-gray-500 group-hover:text-primary line-clamp-2">
