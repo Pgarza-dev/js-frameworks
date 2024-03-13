133e7267312b7b5a09236c3f1f502e2cc6a4511b
Pablo Gaza <p.garza1983@gmail.com>
2024-03-13 20:53:17 +0100

    Update dependencies and improve code formatting in checkout page

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -2,13 +2,16 @@
 import useProductStore from "@/store/cart";
 
 function Checkout() {
-    const clearCart = useProductStore((state) => state.clearCart);
-    return (
-        <div>
-            <h1>this is the checkout page</h1>
-            <button onClick={clearCart} className="border border-black px-4 py-1">clear carts</button>
-        </div>
-    );
+  const { cart, clearCart } = useProductStore();
+
+  return (
+    <div>
+      <h1>this is the checkout page</h1>
+      <button onClick={clearCart} className="border border-black px-4 py-1">
+        clear carts
+      </button>
+    </div>
+  );
 }
 
 export default Checkout;
