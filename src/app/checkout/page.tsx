5d64ea72146bdd3e942c44a848e5279c419343b8
Pablo Gaza <p.garza1983@gmail.com>
2024-03-14 12:11:23 +0100

    Add quantity and description to cart items

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -4,9 +4,17 @@ import { ProductDetailsProps } from "../products/[productsId]/page";
 import { useEffect } from "react";
 import { Product } from "@/app/products/Products";
 import Image from "next/image";
+import { BiPlus } from "react-icons/bi";
+import { BiMinus } from "react-icons/bi";
 
 function Checkout() {
-  const { cart, clearCart, deleteProductFromCart } = useProductStore();
+  const {
+    cart,
+    addToCart,
+    clearCart,
+    deleteProductFromCart,
+    getTotalNumberOfItemsInCart,
+  } = useProductStore();
 
   return (
     <div>
@@ -16,23 +24,36 @@ function Checkout() {
         className="border border-black px-4 py-1 uppercase">
         empty cart
       </button>
-      {cart.map((item) => (
-        <div key={item.id}>
-          <h2>{item.title}</h2>
-          <Image
-            src={item.image.url}
-            alt={item.title}
-            width={100}
-            height={100}
-          />
-          <p>{item.price}</p>
-          <button
-            onClick={() => deleteProductFromCart(item.id)}
-            className="border border-black px-4 py-1 uppercase">
-            remove from cart
-          </button>
-        </div>
-      ))}
+      {cart.map((item) => {
+        return (
+          <div key={item.id}>
+            <h2>{item.title}</h2>
+            <Image
+              src={item.image.url}
+              alt={item.title}
+              width={100}
+              height={100}
+            />
+            <p>{item.price}</p>
+            <p>{item.quantity}</p>
+            <p>{item.description}</p>
+            <button>
+              <BiPlus
+                onClick={() => addToCart(item.id)}
+                className="border border-red-500"
+              />
+            </button>
+            <button onClick={() => deleteProductFromCart(item.id)}>
+              <BiMinus className="border border-red-500" />
+            </button>
+            {/* <button
+              onClick={() => deleteProductFromCart(item.id)}
+              className="border border-black px-4 py-1 uppercase">
+              remove from cart
+            </button> */}
+          </div>
+        );
+      })}
     </div>
   );
 }
