8f19b89aae5d7b09544faa3b6d0a07e51526d36f
Pablo Gaza <p.garza1983@gmail.com>
2024-03-14 08:58:10 +0100

    Add link to checkout page in header / .toFixed added to total price

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,11 +6,12 @@ import { BsCart } from "react-icons/bs";
 import { GoSearch } from "react-icons/go";
 import { RiUserShared2Line } from "react-icons/ri";
 import useProductStore from "@/store/cart";
+import Link from "next/link";
 
 export default function Header() {
-  const cart = useProductStore((state) => state.cart);
+  const { cart, getCartTotal } = useProductStore();
   console.log({ cart });
-  
+
   return (
     <header className="bg-lightModeBody h-20 top-0 sticky z-50">
       <Container className="h-full flex items-center md:gap-x-5 justify-between md:justify-start">
@@ -30,13 +31,15 @@ export default function Header() {
           <p className="text-sm ps-1">Login/Register</p>
         </div>
         {/* cart button */}
-        <div className="relative bg-lightModeText rounded-full text-lightText flex items-center justify-center gap-x-1 px-3 py-1.5">
-          <BsCart className="text-xl text-lightText" />
-          <p className="text-sm ps-1">$0.00</p>
-          <span className="bg-lightModeBody shadow-xl shadow-black text-orange-500 rounded-full text-sm font-semibold absolute -right-2 -top-1 w-5 h-5 flex items-center justify-center">
-            {cart.length}
-          </span>
-        </div>
+        <Link href="/checkout">
+          <div className="relative bg-lightModeText rounded-full text-lightText flex items-center justify-center gap-x-1 px-3 py-1.5">
+            <BsCart className="text-xl text-lightText" />
+            <p className="text-sm ps-1">{Number(getCartTotal()).toFixed(2)}</p>
+            <span className="bg-lightModeBody shadow-xl shadow-black text-orange-500 rounded-full text-sm font-semibold absolute -right-2 -top-1 w-5 h-5 flex items-center justify-center">
+              {cart.length}
+            </span>
+          </div>
+        </Link>
       </Container>
     </header>
   );
