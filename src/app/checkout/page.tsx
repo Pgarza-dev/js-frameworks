7e0e8f56a5d866076a2a3248c30fb669563cc8cd
Pablo Gaza <p.garza1983@gmail.com>
2024-03-15 12:44:29 +0100

    Refactor code to improve performance and readability

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -22,85 +22,139 @@ function Checkout() {
   } = useProductStore();
 
   return (
-    <div>
-      <Container className="">
-        <div className="grid grid-cols-1 small:grid-cols-[1fr_360px] gap-x-40">
-          <div className="flex flex-row justify-between py-4">
-            <Link href="/">
-              <button className="capitalize">back to products</button>
-            </Link>
-            <button onClick={clearCart} className="capitalize">
-              empty cart
-            </button>
-          </div>
+    <Container className="flex flex-col gap-y-2">
+      <div className="hidden sm:inline-flex md:inline-flex lg:inline-flex items-center justify-between font-semibold bg-white p-2">
+        <p className="w-1/3">Product</p>
+        <p className="w-1/3 flex justify-center items-center ">Quantity</p>
+        <p className="w-1/3 flex justify-end items-center ">Subtotal</p>
+      </div>
+      <div className="flex flex-col gap-2">
+        {cart.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="flex flex-col md:flex-row lg:items-center lg:justify-between bg-white p-2">
+              <div className="flex flex-row items-center gap-x-2 w-full">
+                <div className="w-1/3">
+                  <h2 className="uppercase py-1 text-gray-500">{item.title}</h2>
+                  <Image
+                    src={item.image.url}
+                    alt="product image"
+                    width={500}
+                    height={500}
+                    className="rounded-lg object-cover w-20 h-20 lg:w-32 lg:h-32"
+                  />
+                </div>
+                <div className="p-4 text-center text-pretty text-sm">
+                  <p className="text-gray-500 hidden md:block">
+                    {item.description}
+                  </p>
+                </div>
+              </div>
+              <div className="w-1/3 flex justify-center items-center">
+                <button
+                  onClick={() => addSingleProductToCart(item.id)}
+                  className=" px-1 hover:bg-lightText font-extrabold">
+                  <BiPlus />
+                </button>
+                <span className="px-3">{item.quantity}</span>
+                <button
+                  onClick={() => deleteSingleProductFromCart(item.id)}
+                  className=" px-1 hover:bg-lightText font-extrabold">
+                  <BiMinus />
+                </button>
+              </div>
+              <div className="w-1/3 flex justify-end items-center">
+                {(Number(item.price) * Number(item.quantity)).toFixed(2)}
+                <button
+                  onClick={() => deleteProductFromCart(item.id)}
+                  className="text-lg text-gray-500 px-4 bg-white hover:shadow-sm  hover:text-red-500">
+                  <GoTrash />
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </Container>
 
-          <div className="flex flex-col bg-white p-6 gap-y-6">
-            <table className="w-full">
-              <thead>
-                <tr className="border-b">
-                  <th className="inline-flex">Item</th>
-                  <th>Quantity</th>
-                  <th>Total</th>
-                </tr>
-              </thead>
-              <tbody>
-                {cart.map((item) => {
-                  return (
-                    <tr key={item.id}>
-                      <td>
-                        <div className="flex flex-col gap-x-6 pt-8">
-                          <h2 className="uppercase py-1">{item.title}</h2>
-                          <Image
-                            src={item.image.url}
-                            alt={item.title}
-                            width={200}
-                            height={200}
-                            className="rounded-lg object-cover"
-                          />
-                          <div className="pt-4">
-                            <p className="text-gray-500 hidden md:block">
-                              {item.description}
-                            </p>
-                          </div>
-                        </div>
-                      </td>
+    // <Container className="">
+    //   <div className="grid grid-cols-1 small:grid-cols-[1fr_360px] gap-x-40">
+    //     <div className="flex flex-row justify-between py-4">
+    //       <Link href="/">
+    //         <button className="capitalize">back to products</button>
+    //       </Link>
+    //       <button onClick={clearCart} className="capitalize">
+    //         empty cart
+    //       </button>
+    //     </div>
 
-                      <td>
-                        <div className="flex flex-row items-center justify-center">
-                          <button
-                            onClick={() => addSingleProductToCart(item.id)}
-                            className=" px-1 hover:bg-lightText font-extrabold">
-                            <BiPlus />
-                          </button>
-                          <span className="px-3">{item.quantity}</span>
-                          <button
-                            onClick={() => deleteSingleProductFromCart(item.id)}
-                            className=" px-1 hover:bg-lightText font-extrabold">
-                            <BiMinus />
-                          </button>
-                        </div>
-                      </td>
-                      <td className="ps-6">
-                        {(Number(item.price) * Number(item.quantity)).toFixed(
-                          2
-                        )}
-                      </td>
-                      <td>
-                        <button
-                          onClick={() => deleteProductFromCart(item.id)}
-                          className="text-lg text-gray-500 p-1 bg-white hover:shadow-sm  hover:text-red-500">
-                          <GoTrash />
-                        </button>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </Container>
-    </div>
+    //     <div className="flex flex-col bg-white p-6 gap-y-6">
+    //       <table className="w-full">
+    //         <thead>
+    //           <tr className="border-b">
+    //             <th className="inline-flex">Item</th>
+    //             <th>Quantity</th>
+    //             <th>Total</th>
+    //           </tr>
+    //         </thead>
+    //         <tbody>
+    //           {cart.map((item) => {
+    //             return (
+    //               <tr key={item.id}>
+    //                 <td>
+    //                   <div className="flex flex-col gap-x-6 pt-8">
+    //                     <h2 className="uppercase py-1">{item.title}</h2>
+    //                     <Image
+    //                       src={item.image.url}
+    //                       alt={item.title}
+    //                       width={200}
+    //                       height={200}
+    //                       className="rounded-lg object-cover"
+    //                     />
+    //                     <div className="pt-4">
+    //                       <p className="text-gray-500 hidden md:block">
+    //                         {item.description}
+    //                       </p>
+    //                     </div>
+    //                   </div>
+    //                 </td>
+
+    //                 <td>
+    //                   <div className="flex flex-row items-center justify-center">
+    //                     <button
+    //                       onClick={() => addSingleProductToCart(item.id)}
+    //                       className=" px-1 hover:bg-lightText font-extrabold">
+    //                       <BiPlus />
+    //                     </button>
+    //                     <span className="px-3">{item.quantity}</span>
+    //                     <button
+    //                       onClick={() => deleteSingleProductFromCart(item.id)}
+    //                       className=" px-1 hover:bg-lightText font-extrabold">
+    //                       <BiMinus />
+    //                     </button>
+    //                   </div>
+    //                 </td>
+    //                 <td className="ps-6">
+    //                   {(Number(item.price) * Number(item.quantity)).toFixed(
+    //                     2
+    //                   )}
+    //                 </td>
+    //                 <td>
+    //                   <button
+    //                     onClick={() => deleteProductFromCart(item.id)}
+    //                     className="text-lg text-gray-500 p-1 bg-white hover:shadow-sm  hover:text-red-500">
+    //                     <GoTrash />
+    //                   </button>
+    //                 </td>
+    //               </tr>
+    //             );
+    //           })}
+    //         </tbody>
+    //       </table>
+    //     </div>
+    //   </div>
+    // </Container>
   );
 }
 
