e3c19ebdcaf5ae2ceb8a4bbd3a6b17e1eea95175
Pablo Gaza <p.garza1983@gmail.com>
2024-03-15 13:45:04 +0100

    Add FormattedPrice component to Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,6 +7,7 @@ import { GoSearch } from "react-icons/go";
 import { RiUserShared2Line } from "react-icons/ri";
 import useProductStore from "@/store/cart";
 import Link from "next/link";
+import FormattedPrice from "../formattedPrices/FormattedPrices";
 
 export default function Header() {
   const { cart, getCartTotal, getTotalNumberOfItemsInCart } = useProductStore();
@@ -34,7 +35,8 @@ export default function Header() {
         <Link href="/checkout">
           <div className="relative bg-lightModeText rounded-full text-lightText flex items-center justify-center gap-x-1 px-3 py-1.5">
             <BsCart className="text-xl text-lightText" />
-            <p className="text-sm ps-1">{Number(getCartTotal()).toFixed(2)}</p>
+            {/* <p className="text-sm ps-1">{Number(getCartTotal()).toFixed(2)}</p> */}
+            <FormattedPrice amount={getCartTotal()} />
             <span className="bg-lightModeBody shadow-xl shadow-black text-orange-500 rounded-full text-sm font-semibold absolute -right-2 -top-1 w-5 h-5 flex items-center justify-center">
               {cart.length}
             </span>
