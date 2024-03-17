4d2e88137b3fc5b682c71cad41d77c715cd761b7
Pablo Gaza <p.garza1983@gmail.com>
2024-03-17 10:48:17 +0100

    Add useState and useFetch to Header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,15 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import useFetch from "@/hooks/data";
+import { API_PRODUCTS } from "@/shared/apis";
 import Container from "../container/Container";
 import Logo from "../logo/Logo";
 import { BsCart } from "react-icons/bs";
-import { GoSearch } from "react-icons/go";
 import { RiUserShared2Line } from "react-icons/ri";
 import useProductStore from "@/store/cart";
 import Link from "next/link";
 import FormattedPrice from "../formattedPrices/FormattedPrices";
+import SearchBar from "../searchBar/SearchBar";
 
 export default function Header() {
   const { cart, getCartTotal, getTotalNumberOfItemsInCart } = useProductStore();
@@ -18,14 +20,7 @@ export default function Header() {
       <Container className="h-full flex flex-row items-center md:gap-x-5 justify-between md:justify-start">
         <Logo />
         {/* Search bar */}
-        <div className="group w-full border-2 hidden md:flex items-center gap-x-1 bg-white rounded-full px-4 py-1.5 focus-within:border-lightModePrimary">
-          <GoSearch className="text-lightModeText group-focus-within:text-darkText duration-200 " />
-          <input
-            type="text"
-            placeholder="Search for products"
-            className="text-lightModeText placeholder:text-sm placeholder:text-gray-400 flex-1 outline-none bg-white focus:placeholder:text-darkText focus:bg-white"
-          />
-        </div>
+        <SearchBar />
         {/* Login Button */}
         <div className=" text-lightModeText border-2 hidden bg-white sm:flex items-center justify-center p-1.5 rounded-full hover:border-2 hover:border-lightModePrimary cursor-pointer hover:text-lightModeText/30 duration-200">
           <RiUserShared2Line className="text-xl text-lightModeText" />
---
+++ src/components/searchBar/SearchBar.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { GoSearch } from "react-icons/go";
+import useFetch from "@/hooks/data";
+import { API_PRODUCTS } from "@/shared/apis";
+import Link from "next/link";
+
+export default function SearchBar() {
+  const { data = { data: [] } } = useFetch(API_PRODUCTS);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = event.target.value;
+
+    if (searchTerm.length > 0) {
+      const results = data.data.filter((product: { title: string }) => {
+        return product.title.toLowerCase().includes(searchTerm.toLowerCase());
+      });
+
+      setFilteredProducts(results);
+    } else {
+      setFilteredProducts([]);
+    }
+  };
+
+  const handleLinkClick = () => {
+    setFilteredProducts([]);
+  };
+
+  return (
+    <form className="relative group w-full border-2 hidden md:flex items-center gap-x-1 bg-white rounded-full px-4 py-1.5 focus-within:border-lightModePrimary">
+      <div>
+        <button type="submit" className="relative">
+          <GoSearch className="text-lightModeText group-focus-within:text-darkText duration-200 " />
+        </button>
+        <input
+          onChange={handleSearch}
+          id="searchInput"
+          type="search"
+          placeholder="Search for products"
+          className="text-lightModeText placeholder:text-sm placeholder:text-gray-400 flex-1 outline-none bg-white focus:placeholder:text-darkText focus:bg-white
+                "
+        />
+      </div>
+      {filteredProducts.length > 0 && (
+        <div className="absolute border top-10 p-4 bg-white w-full rounded left-1/2 -translate-x-1/2 flex flex-col gap-2">
+          {filteredProducts.map((product: { id: string; title: string }) => (
+            <div className="border-b-2 text-gray-500 hover:text-black">
+              <Link href={`/products/${product.id}`} key={product.id}>
+                <p onClick={handleLinkClick}>{product.title}</p>
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
+    </form>
+  );
+}
