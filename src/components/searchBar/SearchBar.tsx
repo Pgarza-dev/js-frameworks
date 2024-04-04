0e14cbe039e2c8ba9ce219737bbaaddf09b27dde
Pablo Gaza <p.garza1983@gmail.com>
2024-04-04 14:42:17 +0200

    Add image to search results

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -3,18 +3,25 @@ import { GoSearch } from "react-icons/go";
 import useFetch from "@/lib/hooks/data";
 import { API_PRODUCTS } from "@/shared/apis";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function SearchBar() {
   const { data = { data: [] } } = useFetch(API_PRODUCTS);
+
   const [filteredProducts, setFilteredProducts] = useState([]);
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     const searchTerm = event.target.value;
 
     if (searchTerm.length > 0) {
-      const results = data.data.filter((product: { title: string }) => {
-        return product.title.toLowerCase().includes(searchTerm.toLowerCase());
-      });
+      const results = data.data.filter(
+        (product: { title: string; image: { url: string; alt: string } }) => {
+          return (
+            product.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
+            product.image.url
+          );
+        }
+      );
 
       setFilteredProducts(results);
     } else {
@@ -44,16 +51,30 @@ export default function SearchBar() {
       </div>
       {filteredProducts.length > 0 && (
         <div className="absolute border top-10 p-4 bg-white w-full rounded left-1/2 -translate-x-1/2 flex flex-col gap-2">
-          {filteredProducts.map((product: { id: string; title: string }) => (
-            <div
-              className="border-b-2 text-gray-500 hover:text-black"
-              key={product.id}
-            >
-              <Link href={`/products/${product.id}`}>
-                <p onClick={handleLinkClick}>{product.title}</p>
-              </Link>
-            </div>
-          ))}
+          {filteredProducts.map(
+            (product: {
+              id: string;
+              title: string;
+              image: { url: string; alt: string };
+            }) => (
+              <div
+                className="border-b-2 text-gray-500 hover:text-black"
+                key={product.id}>
+                <Link href={`/products/${product.id}`}>
+                  <div onClick={handleLinkClick} className="flex items-center">
+                    <Image
+                      src={product.image.url}
+                      alt={product.title}
+                      width={40}
+                      height={40}
+                      className="w-auto h-auto"
+                    />
+                    <p className="ml-2">{product.title}</p>
+                  </div>
+                </Link>
+              </div>
+            )
+          )}
         </div>
       )}
     </form>
