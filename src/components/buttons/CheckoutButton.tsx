5ecfc7955a7a322bcd94950fe0a15b9676d87c6d
Pablo Gaza <p.garza1983@gmail.com>
2024-04-07 17:05:00 +0200

    add clear cart functionality after proceed to checkout button

--- src/components/buttons/CheckoutButton.tsx
+++ src/components/buttons/CheckoutButton.tsx
@@ -1,9 +1,18 @@
 import Link from "next/link";
+import useProductStore from "@/store/cart";
 
 export default function CheckoutButton() {
+  const { clearCart } = useProductStore();
+
+  function handleClearCart() {
+    clearCart();
+  }
+
   return (
     <Link href={"/success"}>
-      <button className="text-lightText bg-primary mt-4 py-3 px-6 cursor-pointer hover:bg-green-700 duration-300">
+      <button
+        onClick={handleClearCart}
+        className="text-lightText bg-primary mt-4 py-3 px-6 cursor-pointer hover:bg-green-700 duration-300">
         Proceed to checkout
       </button>
     </Link>
